f579e2a73e93a2e594e39b09a17f7b6778c7673b
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-02 16:45:47 -0800

    Script correction

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(97, 123):
-    a = chr(i)
-    b = str(a)
+        a = chr(i)
+        b = str(a)
 
-    if (c == b):
-        return (True)
-    else:
-        return (False)
+        if (c == b):
+            return (True)
+        else:
+            return (False)
