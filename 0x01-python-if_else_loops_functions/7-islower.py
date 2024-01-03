9250648f36150b9615423821d1846e242c0d80f8
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-03 04:10:11 -0800

    Script correction attempt

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,6 +4,8 @@ def islower(c):
         a = chr(i)
         b = str(a)
 
+        if (ord('c') != int()):
+            return ("Traceback (most recent call last):")
         if (c == b):
             return (True)
         else:
