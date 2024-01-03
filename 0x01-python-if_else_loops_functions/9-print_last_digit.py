02bfc47bc374e23be8a2739a225c8dc83cd83f9d
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-02 17:27:04 -0800

    Script fix attempt

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if (number != range(-200, 200):
-            break
-    else:
-        x = str(number)
-        print(x[-1], end="")
-        return (x[-1])
+    x = str(number)
+    print(x[-1], end="")
+    return (x[-1])
