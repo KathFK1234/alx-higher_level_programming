accb3aae0b2a1499ce80206c5f2c47d1e84ded50
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-03 03:16:46 -0800

    Script correction

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-file = open("100-write.py", "w")
-file.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
-file.close
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
