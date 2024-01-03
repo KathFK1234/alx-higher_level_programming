31d31692dbb5758eb7ad5bf6bd383b6febbd3d4e
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-03 05:13:54 -0800

    Script correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,5 @@ def uppercase(str):
 
         if (x >= 97 and x <= 122):
             x -= 32
-            print("{}\n".format(chr(x)), end="")
+            print("{}".format(chr(x)), end="")
+            print("\n")
