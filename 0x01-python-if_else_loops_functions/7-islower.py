2a9b8f31d4dd6314a5972ebcc78585be7d58b345
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-02 16:57:29 -0800

    Fix attempt

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,11 +3,8 @@ def islower(c):
     for i in range(97, 123):
         a = chr(i)
         b = str(a)
-    for x in range(65, 91):
-        y = chr(x)
-        z = str(y)
 
         if (c == b):
             return (True)
-        elif (c == z):
+        else:
             return (False)
