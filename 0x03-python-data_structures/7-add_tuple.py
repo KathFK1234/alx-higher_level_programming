818433da8eaa1e0b64b04bbb3d180a52fdcd43d7
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-08 22:28:58 -0800

    Script attempt

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    for x in tuple_a[0:1]:
+        for y in tuple_b[0:1]:
+            c = int(tuple_a[0] + tuple_b[0])
+            d = int(tuple_a[1] + tuple_b[1])
+            tuple_new = (c,d)
+            return (tuple_new)
