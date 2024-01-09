b54d58325fb40849069d61a850b5a731c5f699da
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-09 02:06:47 -0800

    Pending correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     for x in my_string:
         if (x == "c"):
-            new_str = ''.join(char for char in my_string if char != x and char != x.upper)
+            new_str = ''.join(char for char in my_string if char != x and char != x.upper())
             print(new_str)
