e7bfdebefd98a2722d3cd21cc88f4cd8590b2a57
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-09 23:57:46 -0800

    Correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     if (my_string is None):
         return None
     new_str = ''.join(
-            char for char in my_string 
+            char for char in my_string
             if char != "c" and char != "C"
             )
     return (new_str)
