8d9507ef7dbe8ef660219fea763534f599137028
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-09 23:32:30 -0800

    Ha

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
+    x = my_list.copy()
     if (idx >= len(my_list) or idx < 0):
-        return (new_list)
+        return (x)
     else:
         my_list[idx] = element
         print(my_list)
