f9e73790e085d2621f290e24ae8deea391f6198e
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-15 06:20:26 -0800

    Script attempt

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = []
+    for x in my_list:
+        if x == search:
+            new_list.append(replace)
+        else:
+            new_list.append(x)
+
+    print(new_list)
