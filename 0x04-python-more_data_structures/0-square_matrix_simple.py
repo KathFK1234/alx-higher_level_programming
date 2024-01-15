484c4261011c6ec4639a66f05a2d508cf5f498d7
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-01-15 05:04:41 -0800

    Script attempt

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    x = len(matrix)
+    y = len(matrix[0])
+
+    for i in range(x):
+        for j in range(y):
+            matrix[i][j] = matrix[i][j] ** 2
+
+    for x in matrix:
+        return (x)
