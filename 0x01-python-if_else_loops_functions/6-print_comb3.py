f3eda3953152fc65283fc1eb242f41d9676b5895
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-03-13 17:55:05 +0300

    Cleanup

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,6 @@ for i in range(10):
     for j in range(i + 1, 10):
         if i == j:
             continue
-        if (str(i) + str(j))[0] == (str(j) + str(i))[0]:
-            continue
         else:
             print("{}".format(str(i) + str(j)), end=", "
                   if str(i) + str(j) != "89" else "\n")
