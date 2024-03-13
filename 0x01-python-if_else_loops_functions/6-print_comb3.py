c726640531dc17a9c46f9ae69bcd5da02d9cb0dc
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-03-13 12:03:16 +0300

    Correction

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == j:
+            continue
+        if (str(i) + str(j))[0] == (str(j) + str(i))[0]:
+            continue
+        else:
+            print(str(i) + str(j), end=", " if str(i) + str(j) != "89" else "")
