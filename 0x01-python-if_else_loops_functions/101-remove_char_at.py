d626989eb338d120f07e0ebcd59a786fe7695c01
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-03-16 17:38:00 +0300

    Correction

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    item = str.replace(str[n], "")
-    return(item)
+    if n < len(str):
+        item = str.replace(str[n], "")
+        return (item)
+    else:
+        return (str)
