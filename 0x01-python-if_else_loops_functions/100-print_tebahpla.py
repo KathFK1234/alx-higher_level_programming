35427b808c566b8365520ea192e2f27409069f76
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-03-16 17:16:33 +0300

    Trial

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+letters = []
+for value in range(122, 96, -1):
+    if value % 2 == 0:
+        letter = chr(value)
+    else:
+        letter = chr(value).upper()
+    letters = "".join(letter)
+    print(letters, end="")
