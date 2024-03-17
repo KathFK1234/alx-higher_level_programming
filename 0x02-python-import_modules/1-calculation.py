09584cb5b7924f5639e5c0f7684e2c428c1c63d6
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-03-17 08:44:24 +0300

    Script attempts

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, calculator_1.add))
+    print("{} - {} = {}".format(a, b, calculator_1.sub))
+    print("{} * {} = {}".format(a, b, calculator_1.mul))
+    print("{} / {} = {}".format(a, b, calculator_1.div))
