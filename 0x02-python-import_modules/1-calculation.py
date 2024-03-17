4a81015c0c60042ca76859f9a9cb8d4ac9bb660c
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-03-17 08:46:24 +0300

    Correction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    import calculator_1 as calc
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, calculator_1.add))
-    print("{} - {} = {}".format(a, b, calculator_1.sub))
-    print("{} * {} = {}".format(a, b, calculator_1.mul))
-    print("{} / {} = {}".format(a, b, calculator_1.div))
+    print("{} + {} = {}".format(a, b, calc.add))
+    print("{} - {} = {}".format(a, b, calc.sub))
+    print("{} * {} = {}".format(a, b, calc.mul))
+    print("{} / {} = {}".format(a, b, calc.div))
